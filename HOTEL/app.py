8f72258cc842af0e10821dbf5a8554a26c1ac455
dhruvishadolar <dhruvishadolar@gmail.com>
2025-05-25 13:57:25 +0530

    Update app.py

--- HOTEL/app.py
+++ HOTEL/app.py
@@ -274,4 +274,4 @@ if __name__ == '__main__':
             ])
             db.session.commit()
         print("Database initialized")
-app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+app.run(debug=True, host='0.0.0.0', port=5000)
